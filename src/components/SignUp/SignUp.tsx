d5f9d8a554ff35f4b063c45c1140436f8fbfbe21
j w <291339419@qq.com>
2019-05-01 00:49:05 +0800

    添加注册页面

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,10 +1,64 @@
 import React, { Component } from 'react'
+import { Input, Icon ,Button } from 'antd';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+import './SignUp.scss'
 
-export default class SignUp extends Component {
+interface ISignUpState {
+  account: string,
+  password: string,
+  passwordConformation: string
+}
+
+export default class SignUp extends Component<any,ISignUpState> {
+  constructor(props:any) {
+    super(props)
+    this.state = {
+      account: '',
+      password: '',
+      passwordConformation: ''
+    }
+  }
+
+  onChangeAccount = (e:any) => {
+    this.setState( { account: e.target.value})
+  }
+  onChangePassword = (e:any) => {
+    this.setState( {password: e.target.value})
+  }
+  onChangePasswordConformation = (e:any) => {
+    this.setState( {passwordConformation: e.target.value})
+  }
+  submit = async () =>{
+    const { account,password,passwordConformation } = this.state
+    try{
+      await axios.post('sign_up/user',{
+        account,
+        password,
+        password_confirmation: passwordConformation
+      })
+    }catch(e){
+      throw new Error(e)
+    }
+  }
+
   render() {
+    const { account,password,passwordConformation } = this.state
     return (
-      <div>
-        <p>hei</p>
+      <div className='SignUp' id='SignUp'>
+        <h1>番茄闹钟</h1>
+        <Input
+          placeholder="请输入用户名"
+          prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+          value={ account }
+          onChange={this.onChangeAccount}
+        />
+        <Input.Password value={password} placeholder="请输入密码" 
+        onChange={this.onChangePassword} />
+        <Input.Password value={passwordConformation} placeholder="请确认密码" 
+        onChange={this.onChangePasswordConformation} />
+        <Button type="primary" className="loginButton" onClick={this.submit}>注册</Button>
+        <p>如果有账号，请<Link to='/Login'>登录</Link>></p>
       </div>
     )
   }
---
+++ src/config/axios.ts
@@ -0,0 +1,39 @@
+import axios from "axios";
+
+const appID = "tC7J5CxEUjZtjNKiRommKHbg"
+const appSecret ="7V1P6bQbWbDWrXTzJiAPTkm8"
+
+const instance = axios.create({
+  baseURL: 'https://gp-server.hunger-valley.com/',
+  headers: {
+    't-app-id': appID,
+    't-app-secret': appSecret
+  }
+});
+
+// 添加请求拦截器
+instance.interceptors.request.use(function (config:any) {
+  // 在发送请求之前做些什么
+  const xToken = localStorage.getItem('x-token')
+  if(xToken){
+    config.headers['Authorization'] = `Bearer' ${xToken}`
+  }
+  return config;
+}, function (error:any) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response:any) {
+  // 对响应数据做点什么
+  if(response.headers['x-token']){
+    localStorage.setItem('x-token',response.headers['x-token'])
+  }
+  return response;
+}, function (error:any) {
+  // 对响应错误做点什么
+  return Promise.reject(error);
+});
+
+export default instance
